24a6d77847d0673da21cac2da449f2a59f59f27c
Farhan Mac <farhan.fadila1717@gmail.com>
2021-11-27 09:47:42 +0700

    Kendaran kickoff

---
+++ src/parking/kendaraan/KendaraanController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking.kendaraan;
+
+import java.util.List;
+import parking.database.DatabaseMethod;
+/**
+ *
+ * @author Farhan Fadila
+ */
+public class KendaraanController implements DatabaseMethod<Kendaraan> {
+    
+    KendaraanClient kendaraanClient = null;
+    
+    public KendaraanController() {
+        if(kendaraanClient == null) {
+            kendaraanClient = new KendaraanClient();
+        }
+    }
+
+    @Override
+    public Kendaraan get(String id) {
+       return kendaraanClient.get(id);
+    }
+
+    @Override
+    public List<Kendaraan> getList() {
+       return kendaraanClient.getList();
+    }
+
+    @Override
+    public void create(Kendaraan object) {
+       kendaraanClient.create(object);
+    }
+
+    @Override
+    public void delete(String id) {
+       kendaraanClient.delete(id);
+    }
+}
